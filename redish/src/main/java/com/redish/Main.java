29d6e27deb7cd94154d2e15d9b4727ca881e9a39
Soumyajyoti Dey <deysoumyajyoti2017@gmail.com>
2025-10-18 19:28:33 +0530

    Implement a basic server for responding with PONG on every command

---
+++ redish/src/main/java/com/redish/Main.java
@@ -0,0 +1,35 @@
+package com.redish;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+  public static void main(String[] args) {
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 6379;
+    try {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setReuseAddress(true);
+      clientSocket = serverSocket.accept();
+
+      OutputStream os = clientSocket.getOutputStream();
+
+      String pongResponse = "+PONG\r\n";
+      os.write(pongResponse.getBytes());
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
+  }
+
+}
